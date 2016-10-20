14f617f6876b02ca9397fe816fa2f9a3b6d3aaa6
Wodonga TAFE <SMO-LearningInnovation@wiot.onmicrosoft.com>
2016-10-20 15:30:50 +1100

    Bundle update for site address.

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -8871,7 +8871,10 @@ angular.module('mm.core.login', [])
     .state('mm_login.site', {
         url: '/site',
         templateUrl: 'core/components/login/templates/site.html',
-        controller: 'mmLoginSiteCtrl'
+        controller: 'mmLoginSiteCtrl',
+		onEnter: function($state) {
+        	$state.go('mm_login.credentials', {siteurl: 'https://online.tafevc.com.au/wodonga'});
+    	}
     })
     .state('mm_login.credentials', {
         url: '/cred',
